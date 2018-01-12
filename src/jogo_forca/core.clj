b9d36700dce4980c72f5ab167de399337b5c422b
Elvis Estevan <elvis.estevan65@gmail.com>
2018-01-12 10:36:43 -0200

    Aula 04

--- src/jogo_forca/core.clj
+++ src/jogo_forca/core.clj
@@ -3,15 +3,38 @@
 
 (def total-de-vidas 6)
 
-(defn perdeu [] (print "Você perdeu"))
+(defn perdeu [] (print "Você perdeu!"))
+(defn ganhou [] (print "Você ganhou!"))
 
-(defn jogo [vidas]
+(defn letras-faltantes [palavra acertos]
+  (remove (fn [letra] (contains? acertos (str letra))) palavra)
+)
+
+(defn acertou-a-palavra-toda? [palavra acertos] 
+  (empty? (letras-faltantes palavra acertos))
+)
+
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra]
+  (.contains palavra chute)
+)
+
+(defn avalia-chute [chute vidas palavra acertos]
+  (if (acertou? chute palavra)
+    (jogo vidas palavra (conj acertos chute))
+    (jogo (dec vidas) palavra acertos)
+  )
+)
+
+(defn jogo [vidas palavra acertos]
   (if (= vidas 0)
     (perdeu)
-    (do
-      (print vidas)
-      (jogo (dec vidas))
+    (if (acertou-a-palavra-toda? palavra acertos)
+      (ganhou)
+      (avalia-chute (le-letra!) vidas palavra acertos)
     )
+
   )
 )
 
