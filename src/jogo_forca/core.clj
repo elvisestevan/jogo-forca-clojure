69c0b8c52e56bfbe831979f56b0c66ce671b4d80
Elvis Estevan <elvis.estevan65@gmail.com>
2018-01-17 09:01:39 -0200

    Aula 05 - Recursão de cauda

--- src/jogo_forca/core.clj
+++ src/jogo_forca/core.clj
@@ -20,21 +20,23 @@
   (.contains palavra chute)
 )
 
-(defn avalia-chute [chute vidas palavra acertos]
-  (if (acertou? chute palavra)
-    (jogo vidas palavra (conj acertos chute))
-    (jogo (dec vidas) palavra acertos)
-  )
-)
-
 (defn jogo [vidas palavra acertos]
-  (if (= vidas 0)
-    (perdeu)
-    (if (acertou-a-palavra-toda? palavra acertos)
-      (ganhou)
-      (avalia-chute (le-letra!) vidas palavra acertos)
+  (cond 
+    (= vidas 0) (perdeu)
+    (acertou-a-palavra-toda? palavra acertos) (ganhou)
+  :else
+    (let [chute (le-letra!)]
+      (if (acertou? chute palavra)
+        (do
+          (println "Acertou!")
+          (recur vidas palavra (conj acertos chute))
+        )
+        (do
+          (println "Eroooow!")
+          (recur (dec vidas) palavra acertos)
+        )
+      )
     )
-
   )
 )
 
@@ -44,6 +46,24 @@
     (if (= n 1)
       1
       (+ (fib (- n 1)) (fib (- n 2)))
+    )
+  )
+)
+
+(defn fibrecur[n]
+  (loop [a 1 b 1 numero 2]
+    (if 
+      (= numero n) b
+      (recur b (+ a b) (inc numero))
+    )    
+  )
+)
+
+(defn soma[n]
+  (loop [contador 1 soma 0]
+    (if 
+      (> contador n) soma
+      (recur (inc contador) (+ contador soma))
     )
   )
 )
